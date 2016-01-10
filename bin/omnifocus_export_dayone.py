d7aaddf3ff1d30ace0534a0cc29850b07946d68f
NSDont <haodewanan@gmail.com>
2016-01-10 10:41:30 +0800

    Update Omnifocus export Dayone
    1. add docopt (support args parser)
    2. add logging
    3. add --show

--- bin/omnifocus_export_dayone.py
+++ bin/omnifocus_export_dayone.py
@@ -1,15 +1,52 @@
 #!/usr/local/bin/python3
+"""OmniFocus export to Dayone.
+
+Usage:
+  omnifocus_export_dayone.py
+  omnifocus_export_dayone.py <date> [--show]
+  omnifocus_export_dayone.py (-s | --show)
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  -s --show     Only echo to screen.
+
+"""
 import sys
 import html
 import sqlite3
+import logging
+import logging.handlers
 
 from pync import Notifier
+from docopt import docopt
 from os.path import expanduser
 from datetime import datetime, timedelta
 
 database_path = expanduser('~/Library/Containers/com.omnigroup.OmniFocus2/'
                            'Data/Library/Caches/com.omnigroup.OmniFocus2/'
                            'OmniFocusDatabase2')
+
+logger = logging.getLogger('omnifocus_export')
+
+
+def setup_logging():
+    LOG_FILE = '/tmp/omnifocus_export.log'
+    fmt = '%(asctime)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+
+    handler = logging.handlers.RotatingFileHandler(
+        LOG_FILE, maxBytes=1024 * 1024, backupCount=5)
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.DEBUG)
+    logger.addHandler(console_handler)
+
+    logger.setLevel(logging.DEBUG)
 
 
 def generate_md(data, tag):
@@ -25,7 +62,7 @@ def generate_md(data, tag):
 
     if tag in {'Weekly', 'Monthly'}:
         plan_summer = ('\n### {} Plan\n\n#### Finish Plan\n\n'
-                       '#### Process Plan\n\n#### Raw Plan')
+                       '#### Process Plan\n\n#### Raw Plan\n')
         plan_summer = plan_summer.format(tag)
 
     return base_fmt.format(tasks=tasks, plan_summer=plan_summer)
@@ -56,7 +93,7 @@ def export_to_dayone(md, now, tag):
         f.write(content.encode())
 
 
-def main(start_ts, end_ts, now, tag='Daily'):
+def do(start_ts, end_ts, now, tag='Daily', only_show=False):
     conn = sqlite3.connect(database_path)
     cur = conn.cursor()
 
@@ -96,38 +133,60 @@ def main(start_ts, end_ts, now, tag='Daily'):
 
     md = generate_md(data, tag)
 
-    export_to_dayone(md, now, tag)
+    if only_show:
+        print(md)
+    else:
+        export_to_dayone(md, now, tag)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
+def main(args):
+    if args['<date>']:
         try:
-            now = datetime.strptime(sys.argv[1], '%Y.%m.%d')
+            now = datetime.strptime(args['<date>'], '%Y.%m.%d')
         except ValueError:
             print('时间串格式错误，比如: 2015.11.17')
             sys.exit(1)
     else:
         now = datetime.now()
+    only_show = args['--show']
     base_timestamp = datetime(2001, 1, 1).timestamp()
     today = datetime(now.year, now.month, now.day)
     tomorrow = today + timedelta(1)
 
     if now.weekday() == 5:
         # 生成周报
+        logger.info('Start generate weekly...')
         today_timestamp = (today - timedelta(6)).timestamp() - base_timestamp
         tomorrow_timestamp = tomorrow.timestamp() - base_timestamp
-        main(today_timestamp, tomorrow_timestamp, now, 'Weekly')
+        do(today_timestamp, tomorrow_timestamp, now, 'Weekly',
+            only_show=only_show)
+        logger.info('Finish generate weekly...')
 
     tomorrow = now + timedelta(days=1)
     if tomorrow.month > now.month or (now.month == 12 and tomorrow.month == 1):
         # 生成月报
+        logger.info('Start generate monthly...')
         today_timestamp = (today - timedelta(30)).timestamp() - base_timestamp
         tomorrow_timestamp = tomorrow.timestamp() - base_timestamp
-        main(today_timestamp, tomorrow_timestamp, now, 'Monthly')
+        do(today_timestamp, tomorrow_timestamp, now, 'Monthly',
+           only_show=only_show)
+        logger.info('Finish generate monthly...')
 
     # 生成日报
+    logger.info('Start generate daily...')
     today_timestamp = today.timestamp() - base_timestamp
     tomorrow_timestamp = tomorrow.timestamp() - base_timestamp
-    main(today_timestamp, tomorrow_timestamp, now, 'Daily')
+    do(today_timestamp, tomorrow_timestamp, now, 'Daily', only_show=only_show)
+    logger.info('Finish generate daily...')
 
-    Notifier.notify('Export DayOne Done.', title='Omnifocus Statistics')
+    if not only_show:
+        Notifier.notify('Export DayOne Done.', title='Omnifocus Statistics')
+
+
+if __name__ == '__main__':
+    setup_logging()
+    arguments = docopt(__doc__, version='0.1.1')
+
+    logger.info('--------START------')
+    main(arguments)
+    logger.info('--------END--------')
