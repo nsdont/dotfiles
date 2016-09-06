0aed86412d387b8584519c844da311497a948646
NSDont <haodewanan@gmail.com>
2016-09-06 11:59:07 +0800

    Modify fzf install script

--- script/install_other_package.sh
+++ script/install_other_package.sh
@@ -5,7 +5,7 @@ install_fzf () {
   then
     info 'installing fzf'
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install --no-update-rc --key-bindings --completion
+    ~/.fzf/install --all
   fi
 }
 
